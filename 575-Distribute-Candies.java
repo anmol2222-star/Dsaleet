f86c520503f419d2358adaf90cacf5b60423a84c
anmol2222-star <as7343406@gmail.com>
2026-08-31 23:45:56 +0530

    [575] [Time Beats: 92.11%] [Memory Beats: 6.31%] - LeetPush

---
+++ 575-Distribute-Candies.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int distributeCandies(int[] candyType) {
+      HashSet <Integer> set=new HashSet<Integer>();
+        for(int i=0;i<candyType.length;i++){
+            set.add(candyType[i]);
+        }
+        return Math.min(set.size(),candyType.length/2);
+    }
+}
